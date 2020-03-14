1da155f64a905d27f2f69ab8aa54c469195d5001
Patrick Parham <34312992+PatrickParham@users.noreply.github.com>
2020-03-14 16:28:51 -0400

    Project Parts 2 & # Update
    Project Parts 2 & # Update

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,8 @@ WORKDIR /app
 
 COPY . .
 
+COPY main.py /app
+
+COPY requirements.txt /app/requirements.txt
+
 RUN pip install -r requirements.txt
--- main.py
+++ main.py
@@ -1,40 +1,64 @@
+########################
+### Install Packages ###
+########################
+
 import argparse
 import sodapy
 import json
+import pprint
 import pandas
 import os
 from sodapy import Socrata
 from requests import get, HTTPError
+from elasticsearch import Elasticsearch
+from datetime import datetime
+
+
+########################
+#### Elastic Search ####
+########################
+
+def create_and_update_index(index_name, doc_type):
+    es = Elasticsearch()
+    try:
+        es.indices.create(index=index_name)
+        es.indices.put_mapping(index=index_name, doc_type=doc_type)
+    except:
+        pass
+    return es
+
+########################
+###### API Inputs ######
+########################
 
 API_BASE = 'data.cityofnewyork.us'
 DATASET = 'nc67-uf89'
 
+########################
+##### NYC API Pull #####
+########################
 
-parser = argparse.ArgumentParser(description='Return NYC Parking Violations')
-parser.add_argument('--APP_KEY', type=str, required=True,help="NYC Open Data Unique Token")
-parser.add_argument('--page_size', type=int, required=True, help="Number of Violation Entries to Return")
-parser.add_argument('--output', type=int, help="Number of Violation Entries to Return")
+#### NYC_PARKING Funtion ####
 
-args = parser.parse_args()
-
-def nyc_parking(APP_KEY,page_size, outputcsv = 1) -> dict:
+def nyc_parking(APP_KEY, page_size, elastics, output) -> dict:
 
     try:
-        if outputcsv == 1:
-            API_KEY = os.environ.get(APP_KEY)
-            client = Socrata(API_BASE,API_KEY)
-            r = client.get(DATASET,limit=page_size)
-            dfoutput = pandas.DataFrame(r)
-            dfoutput.to_csv('main_output.csv', index=False)
-            r = json.dumps(r)
-            print(r)
-
-        else: 
-            API_KEY = os.environ.get(APP_KEY)
-            client = Socrata(API_BASE,API_KEY)
-            r = client.get(DATASET,limit=page_size)
-            r = json.dumps(r)
-            print(r)
+        API_KEY = os.environ.get(APP_KEY)
+        client = Socrata(API_BASE,API_KEY)
+        citations = client.get(DATASET,limit=page_size)
+        for citation in citations:
+            citation['issue_date'] = datetime.strptime(citation['issue_date'],'%m/%d/%Y')
+            if elastics:
+                pprint.pprint(citation)
+                es = create_and_update_index('bigdata1_index', 'bigdata1')
+                res = es.index(index='bigdata1_index', doc_type='bigdata1', body=citation)
+                print(res)
+            if output:
+                pprint.pprint(citation)
+                dfoutput = pandas.DataFrame(citations)
+                dfoutput.to_csv('main_output.csv', index=False)
+            else:
+                pprint.pprint(citation)
 
     except HTTPError as e:
         print(f"Failed to make API call: {e}")
@@ -46,6 +70,14 @@ def nyc_parking(APP_KEY,page_size, outputcsv = 1) -> dict:
     except Exception as e:
         print(f"Something went wrong: {e}")
         raise
-        
+
+###### Argparse ########
+
 if __name__ == "__main__":
-    print(nyc_parking(args.APP_KEY,args.page_size,args.output))
+    parser = argparse.ArgumentParser(description='Return NYC Parking Violations')
+    parser.add_argument('--APP_KEY', type=str, required=True,help="NYC Open Data Unique Token")
+    parser.add_argument('--page_size', type=int, required=True, help="Number of Violation Entries to Return")
+    parser.add_argument("--elastics", default=None, help="Push to ElasticSearch")
+    parser.add_argument("--output", default=None, help="Send CSV File to Local Directory")
+    args = parser.parse_args()
+    nyc_parking(args.APP_KEY,args.page_size,args.elastics,args.output)
